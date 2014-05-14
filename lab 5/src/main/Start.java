79b3c05137ed2a3d0a88eddfdbec59e23998c1e0
Hans Rietz <dat12hri@student.lth.se>
2014-05-14 13:25:08 +0200

    gjorde mer

--- lab 5/src/main/Start.java
+++ lab 5/src/main/Start.java
@@ -5,15 +5,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Start {
-    String[][] blosum62 = new String[24][24];
-    HashMap<Character,Integer> blosum_Index = new HashMap<Character, Integer>();
-
+    static String[][] blosum62 = new String[24][24];
+    static HashMap<Character,Integer> blosum_Index = new HashMap<Character, Integer>();
+    final static int gap = -4;
+    static int[][] matrix;
     private ArrayList<String> pre(){
         ArrayList<String> inputList = new ArrayList<String>();
         try{
-            BufferedReader blosumReader = new BufferedReader(new FileReader(new File("in filer/BLOSUM62.txt")));
+            BufferedReader blosumReader = new BufferedReader(new FileReader(new File("lab 5/inputFiles/BLOSUM62.txt")));
             String line = "";
             String[] rows;
             String Debug= "";
@@ -21,16 +24,12 @@ public class Start {
             line = blosumReader.readLine();
 
             while(line != null){
-
-                System.out.println(line);
                 rows = line.split(" +");
+                blosum_Index.put(rows[0].trim().charAt(0), j);
                 for(int i = 1; i<25;i++){
                     blosum62[j][i-1] = rows[i].trim();
-                    Debug+=" "+ rows[i];
                 }
                 j=j+1;
-                System.out.println(Debug);
-                Debug = "";
                 line = blosumReader.readLine();
             }
         }
@@ -47,10 +46,10 @@ public class Start {
         return inputList;
 
     }
-    private int allScore(int[][] matrix, char dna_0Component, char dna_1Component){
-        return -1;
 
-    }
+
+
+
     public static void main(String[] args){
         Start start = new Start();
         ArrayList<String> inputList = new ArrayList<String>();
@@ -59,23 +58,87 @@ public class Start {
         for(int i = 0; i<inputList.size()/2; i=i+2){
             speciesList.add(new Species(inputList.get(i),inputList.get(i+1)));
         }
-        char[] dna_0 = speciesList.get(0).name.toCharArray();
-        char[] dna_1 = speciesList.get(1).name.toCharArray();
-        int sizeX = speciesList.get(0).name.length()+1;
-        int sizeY = speciesList.get(1).name.length()+1;
-        int[][] matrix = new int[sizeX][sizeY];
+        char[] dna_0 = speciesList.get(0).dna.toCharArray();
+        char[] dna_1 = speciesList.get(1).dna.toCharArray();
+        int sizeX = speciesList.get(0).dna.length();
+        int sizeY = speciesList.get(1).dna.length();
+        matrix = new int[sizeX+1][sizeY+1];
         for(int i = 0; i<sizeX;i++){
             matrix[i][0] = 0;
         }
         for(int i = 0; i<sizeY;i++){
             matrix[0][i] = 0;
+
         }
-        for(int i = 1; i<sizeX;i++){
-            for(int j = 1; j<sizeY;j++){
+        Set<Character> a = blosum_Index.keySet();
+        Iterator aa = a.iterator();
 
 
+
+        for(int i = 1; i< sizeX; i++){
+            for(int j = 1; j < sizeY;j++){
+                int left,diag,top;
+                left = matrix[i - 1][j]+gap;
+                diag = matrix[i-1][j-1]+ Integer.parseInt(blosum62[blosum_Index.get(dna_0[i-1])][blosum_Index.get(dna_1[j-1])]);
+                top = matrix[i][j-1] + gap;
+
+                int max = Math.max(left,diag);
+                if(max < top){
+                    max = top;
+                }
+                matrix[i][j] = max;
             }
         }
+
+        for(int i = 0; i<4;i++){
+            String debug1 ="";
+            for(int j =0; j < 3; j++){
+
+                debug1 = debug1+matrix[i][j]+" ";
+
+            }
+            System.out.println(debug1);
+        }
+
+
+
+        String x_Dna ="";
+        String y_Dna ="";
+        int i = dna_0.length-1;
+        int j = dna_1.length-1;
+        int superScore = matrix[sizeX-1][sizeY-1];
+        int current_Score = matrix[i][j];
+        int left_Score = matrix[i-1][j];
+        int top_Score = matrix[i][j-1];
+        int diag_Score = matrix[i-1][j-1];
+
+        while((i > 0 && j > 0)){
+            current_Score = matrix[i][j];
+            left_Score = matrix[i-1][j];
+            top_Score = matrix[i][j-1];
+            diag_Score = matrix[i-1][j-1];
+            System.out.println("current_Score ="+current_Score+ " left_Score = "+left_Score+" top_Score = "+top_Score + " diag_Score = "+diag_Score);
+            System.out.println(blosum62[blosum_Index.get(dna_0[i-1])][blosum_Index.get(dna_1[j-1])]);
+            if(current_Score == (diag_Score + Integer.parseInt(blosum62[blosum_Index.get(dna_0[i-1])][blosum_Index.get(dna_1[j-1])]))){
+                x_Dna += dna_0[i-1];
+                y_Dna += dna_1[j-1];
+                i-=1;
+                j-=1;
+            } else if(current_Score == (left_Score+gap)){
+                x_Dna += dna_0[i-1];
+                y_Dna += '-';
+                i-=1;
+            } else if(current_Score == top_Score + gap){
+                x_Dna += '-';
+                y_Dna += dna_1[j-1];
+                j-=1;
+            }
+            System.out.println("i = "+i+" j = "+j);
+
+        }
+        System.out.println(x_Dna+"\n"+y_Dna);
+
+
 
 
 
